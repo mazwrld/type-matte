6555fa9573282ff9a1e8cea0b1b4df17fdce1648
Maz <mazspm@icloud.com>
2023-03-13 04:47:07 -0500

    added logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { Logo } from "~/ui-kit/logo";
 
 const Home: NextPage = () => {
   return (
@@ -7,8 +8,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Type Matte</title>
       </Head>
-      <main className={"min-h-screen"}>
-        <div className="container mx-auto flex items-center justify-center p-6"></div>
+      <main className={"min-h-screen bg-slate-500"}>
+        <div className="container mx-auto flex items-center justify-between p-6">
+          <Logo />
+        </div>
       </main>
     </>
   );
--- src/ui-kit/logo/index.tsx
+++ src/ui-kit/logo/index.tsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <div>
-      <Image src={"/img/sun. svg"} width={25} height={25} alt={"logo"} />
-      <span>Type Matte</span>
+    <div className="flex cursor-pointer select-none items-center">
+      <Image
+        src={"/img/sun.svg"}
+        width={25}
+        height={25}
+        alt={"type-matte-logo"}
+      />
+      <span className={"mr-8 ml-2 text-xl font-bold tracking-wide"}>
+        type-matte
+      </span>
     </div>
   );
 };
